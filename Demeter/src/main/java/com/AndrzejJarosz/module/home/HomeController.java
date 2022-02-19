ee5e25558f80de1e780bb135fbeb0b6931f70db8
SirDraq <redwhitedragon81@gmail.com>
2022-02-19 09:54:13 +0100

    0955

---
+++ Demeter/src/main/java/com/AndrzejJarosz/module/home/HomeController.java
@@ -0,0 +1,12 @@
+package com.AndrzejJarosz.module.home;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping({"/", "/home"})
+    public String getHome(){
+        return "home/index.html";
+    }
+}
